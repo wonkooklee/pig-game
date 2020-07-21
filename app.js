63705407dbb3bf250b8690a8fe78747eca2f93ea
Wonkook Lee <54918022+design-oneook@users.noreply.github.com>
2020-07-21 18:56:36 +0900

    테스트하는 커밋이다 왜!

--- app.js
+++ app.js
@@ -128,7 +128,8 @@ let nextPlayer = function() {
   roundScore = 0;
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   
-  document.getElementById(`current-${activePlayer}`).textContent = '0';
+  document.getElementById(`current-0`).textContent = '0';
+  document.getElementById(`current-1`).textContent = '0';
 
   document.querySelector(`.player-0-panel`).classList.toggle('active');
   document.querySelector(`.player-1-panel`).classList.toggle('active');
