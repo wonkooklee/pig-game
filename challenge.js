a1d91d0aeb9287290b82e063f00f40ea753ec363
Wonkook Lee <54918022+design-oneook@users.noreply.github.com>
2020-07-22 21:48:50 +0900

    additional 6&6 init function

---
+++ challenge.js
@@ -0,0 +1,105 @@
+'use strict';
+
+let scores, roundScore, activePlayer, gamePlaying, dice, diceDOM, lastDice;
+
+let init = function() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+  diceDOM = document.querySelector('.dice');
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  
+  document.querySelector('.player-0-panel').classList.add('active');
+
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  diceDOM.style.display = 'none';
+
+}
+init();
+
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  
+  if (gamePlaying) {
+    
+    dice = Math.floor(Math.random() * 6) + 1;
+    diceDOM.style.display = 'block';
+    diceDOM.src = `dice-${dice}.png`;
+
+    if (dice === 6 && lastDice === 6) {
+      // Player looses score
+      scores[activePlayer] = 0;
+      document.getElementById(`score-${activePlayer}`).textContent = '0';
+      nextPlayer();
+    } else if (dice !== 1 && dice !== 6) {
+      roundScore += dice;
+      document.getElementById(`current-${activePlayer}`).textContent = roundScore;
+      lastDice = 0;
+    } else if (dice === 6) {
+      roundScore += dice;
+      document.getElementById(`current-${activePlayer}`).textContent = roundScore;
+      lastDice = dice;
+    } else {
+      nextPlayer();
+    }
+
+
+  }
+
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  
+  if (gamePlaying) {
+
+    scores[activePlayer] += roundScore;
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+
+      document.getElementById(`name-${activePlayer}`).textContent = 'WINNER!';
+      document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+      document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+      diceDOM.style.display = 'none';
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
+
+  }
+
+});
+
+
+let nextPlayer = function() {
+
+  roundScore = 0;
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  
+  document.getElementById(`current-0`).textContent = '0';
+  document.getElementById(`current-1`).textContent = '0';
+
+  document.querySelector(`.player-0-panel`).classList.toggle('active');
+  document.querySelector(`.player-1-panel`).classList.toggle('active');
+
+  diceDOM.style.display = 'none';
+
+  lastDice = 0;
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
